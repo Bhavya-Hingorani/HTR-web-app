25875c4d3ad38ada7b1c4ea244350c2fe4f986dc
unknown <bhavyahingorani54@gmail.com>
2022-03-28 20:33:26 +0530

    Updating repo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, {useState } from "react";
 import { Button } from 'react-bootstrap'
+import { Base64 } from 'js-base64';
 import './App.css';
 
-var file = null;
+var myFile = null;
 
 function App() {
-  const [selectedFile, fileUploadHandler] = useState(null);
+  const [selectedFile, fileUpdateHandler] = useState(null);
   function fileSelectedHandler(event) {
     console.log(selectedFile);
-    file = event.target.files[0];
+    myFile = event.target.files[0];
+  }
+  function fileUploadHandler(event){
+    fileUpdateHandler(myFile);
+    // saveAs(myFile);
   }
 
   return (
     <div className="App">
       <h1 className='primary'>Welcome</h1>
       <input type='file' onChange={fileSelectedHandler} />
-      <Button className='button'  onClick={() => fileUploadHandler(file)}> Upload file</Button>
+      <Button className='button'  onClick={() => fileUploadHandler()}> Upload file</Button>
       {/* <Button
         title="Press me"
         onPress={fileUploadHandler(file)}
